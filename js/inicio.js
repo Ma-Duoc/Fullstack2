3804213c400f9e4e1183a91f34e2efe4863c19cc
Ma-Duoc <ma.avelloc@duocuc.cl>
2025-09-04 18:45:25 -0400

    ADD inicio.js, registro.html y un estilos.css

---
+++ js/inicio.js
@@ -0,0 +1,11 @@
+// Capturar el botón de registro
+document.querySelector(".btn-outline-primary").addEventListener("click", function() {
+  // Redirigir a la página de registro
+  window.location.href = "registro.html";
+});
+
+
+// Botón Clave Única -> login.html
+document.getElementById("btnClaveUnica").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
